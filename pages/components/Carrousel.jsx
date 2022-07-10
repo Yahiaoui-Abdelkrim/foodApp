915ba641b3d06e1f363e4aee9144daa38f468501
kar-s <karim.yah2024@gmail.com>
2022-07-10 22:40:41 +0200

    Added my project

---
+++ pages/components/Carrousel.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import Image from "next/image";
+import styles from "../../styles/Carroussel.module.css";
+
+const Carrousel = () => {
+  return (
+    <div>
+      <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-inner">
+          <div className={`${styles.image} carousel-item active`}>
+            <Image
+              height={500}
+              width={500}
+              src={
+                "https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+              }
+            />
+          </div>
+          <div className="carousel-item">
+            <Image
+              layout="fill"
+              src={
+                "https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+              }
+            />
+          </div>
+          <div className="carousel-item">
+            <Image
+              height={500}
+              width={500}
+              src={
+                "https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+              }
+            />
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleControls"
+          data-bs-slide="prev"
+        >
+          <span className="carousel-control-prev-icon" aria-hidden="false"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleControls"
+          data-bs-slide="next"
+        >
+          <span className="carousel-control-next-icon" aria-hidden="false"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+export default Carrousel;
